e3d12d745dbc95272465ead5fe4ee462e61eb401
zh8 <zh8@meitu.com>
2021-04-22 16:04:44 +0800

    feat: 处理ctx.body

--- index.js
+++ index.js
@@ -5,13 +5,16 @@ let app = new Koa();
 
 app.use((ctx) => {
   // res.end('hello world')
-  console.log(ctx.req.url)
-  console.log(ctx.request.req.url)
-  console.log(ctx.response.req.url)
-  console.log(ctx.request.url)
-  console.log(ctx.request.path)
-  console.log(ctx.url)
-  console.log(ctx.path)
+  // console.log(ctx.req.url)
+  // console.log(ctx.request.req.url)
+  // console.log(ctx.response.req.url)
+  // console.log(ctx.request.url)
+  // console.log(ctx.request.path)
+  // console.log(ctx.url)
+  // console.log(ctx.path)
+
+  ctx.body = 'hello world';
+  console.log(ctx.body)
 })
 
 app.listen(3000)
