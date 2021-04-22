ba3f8495384ce8e3b6626f7649147f974e38bd80
张桂钧 <172577974@qq.com>
2021-04-22 16:43:37 +0800

    feat: 完成简易koa

--- index.js
+++ index.js
@@ -3,18 +3,37 @@ let Koa = require('./lib/application');
 let app = new Koa();
 
 
-app.use((ctx) => {
-  // res.end('hello world')
-  // console.log(ctx.req.url)
-  // console.log(ctx.request.req.url)
-  // console.log(ctx.response.req.url)
-  // console.log(ctx.request.url)
-  // console.log(ctx.request.path)
-  // console.log(ctx.url)
-  // console.log(ctx.path)
+// app.use((ctx) => {
+//   // res.end('hello world')
+//   // console.log(ctx.req.url)
+//   // console.log(ctx.request.req.url)
+//   // console.log(ctx.response.req.url)
+//   // console.log(ctx.request.url)
+//   // console.log(ctx.request.path)
+//   // console.log(ctx.url)
+//   // console.log(ctx.path)
 
-  ctx.body = 'hello world';
-  console.log(ctx.body)
+//   ctx.body = 'hello world';
+//   console.log(ctx.body)
+// })
+app.use((ctx, next) => {
+  console.log(1)
+  next()
+  console.log(2)
+})
+app.use((ctx, next) => {
+  console.log(3)
+  next()
+  console.log(4)
+})
+app.use((ctx, next) => {
+  console.log(5)
+  next()
+  console.log(6)
+  ctx.body= {
+    code: 1,
+    mag: '11'
+  }
 })
 
 app.listen(3000)
