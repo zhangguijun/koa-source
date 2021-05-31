8ef4d8946eaa843fb0f6181d9598c99f2080e4a0
zh8 <zh8@meitu.com>
2021-05-31 18:27:09 +0800

    feat： 使用Object.defineProperty 替换掉之前的__defineGetter__ 和 __defineSetter__的委托方案

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ let app = new Koa();
 
 // app.use((ctx) => {
 //   // res.end('hello world')
-//   // console.log(ctx.req.url)
-//   // console.log(ctx.request.req.url)
-//   // console.log(ctx.response.req.url)
-//   // console.log(ctx.request.url)
-//   // console.log(ctx.request.path)
-//   // console.log(ctx.url)
-//   // console.log(ctx.path)
+//   console.log(ctx.req.url)
+//   console.log(ctx.request.req.url)
+//   console.log(ctx.response.req.url)
+//   console.log(ctx.request.url)
+//   console.log(ctx.request.path)
+//   console.log(ctx.url)
+//   console.log(ctx.path)
 
 //   ctx.body = 'hello world';
 //   console.log(ctx.body)
