7427f59dd06650be67bb0606ac27eb8fecf991b7
张桂钧 <1725779714@qq.com>
2021-05-31 20:12:41 +0800

    feat: 新增函数注释

--- index.js
+++ index.js
@@ -3,19 +3,19 @@ let Koa = require('./lib/application');
 let app = new Koa();
 
 
-// app.use((ctx) => {
-//   // res.end('hello world')
-//   console.log(ctx.req.url)
-//   console.log(ctx.request.req.url)
-//   console.log(ctx.response.req.url)
-//   console.log(ctx.request.url)
-//   console.log(ctx.request.path)
-//   console.log(ctx.url)
-//   console.log(ctx.path)
+app.use((ctx) => {
+  // res.end('hello world')
+  console.log(ctx.req.url)
+  console.log(ctx.request.req.url)
+  console.log(ctx.response.req.url)
+  console.log(ctx.request.url)
+  console.log(ctx.request.path)
+  console.log(ctx.url)
+  console.log(ctx.path)
 
-//   ctx.body = 'hello world';
-//   console.log(ctx.body)
-// })
+  ctx.body = 'hello world';
+  console.log(ctx.body)
+})
 app.use((ctx, next) => {
   console.log(1)
   next()
